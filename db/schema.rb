c8d566d1c28d0b2e538b065f71469fa879c90c4c
Anike Arni <anikarni@anikarni-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-06-16 17:13:22 -0300

    added rails_12factor to deploy CSS

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140607151947) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "produtos", force: true do |t|
     t.string   "nome"
     t.string   "modelo"
