3631c22d014b62c0830ceb6dce59f9bcbba78fba
Anike Arni <anikarni@anikarni-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-06-16 15:54:33 -0300

    Basic compare app created

---
+++ db/migrate/20140607151947_create_produtos.rb
@@ -0,0 +1,18 @@
+class CreateProdutos < ActiveRecord::Migration
+  def up
+    create_table :produtos do |t|
+      t.string :nome
+      t.string :modelo
+      t.string :preco
+      t.string :tamanhos
+      t.string :cores
+      t.string :quantidade
+      t.text :descricao
+
+      t.timestamps
+    end
+  end
+  def down
+    drop_table :produtos
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
+#   Mayor.create(name: "Emanuel", city: cities.first)
+Produto.create(nome: "Exemplo1", modelo: "Casual", preco: "R$23,40", tamanhos: "P, M, G", cores: "azul, vermelho", quantidade: "5", descricao: "exemplo de produto1")
+Produto.create(nome: "Exemplo2", modelo: "Casual", preco: "R$45,00", tamanhos: "P, G", cores: "amarelo", quantidade: "24", descricao: "exemplo de produto2")
+Produto.create(nome: "Exemplo3", modelo: "Formal", preco: "R$300,00", tamanhos: "P, M, G", cores: "vermelho", quantidade: "5", descricao: "exemplo de produto3")
+Produto.create(nome: "Exemplo4", modelo: "Casual", preco: "R$23,40", tamanhos: "P, M, G", cores: "azul", quantidade: "5", descricao: "exemplo de produto4")
