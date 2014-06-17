697a3374b1763024f2b207e908901507a250fb33
Anike Arni <anikarni@anikarni-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-06-17 12:14:09 -0300

    implemented checkbox functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,12 +20,58 @@
     }, this), 3000));
 
 
-$(function () {    
-  $('#span').hover(function () {
-     $('#header').slideDown('slow');
-  }, function () {
-    $.data(this, 'timer', setTimeout($.proxy(function() {
-      $('#header').slideUp('slow');
-    }, this), 200));
-  });
+$(document).ready(function () {
+    var menu = $('#header')
+    var timeout = 0;
+    var hovering = false;
+    menu.hide();
+
+    $('#span')
+        .on("mouseenter", function () {
+        hovering = true;
+        // Open the menu
+        $('#header')
+            .stop(true, true)
+            .slideDown(400);
+
+        if (timeout > 0) {
+            clearTimeout(timeout);
+        }
+    })
+        .on("mouseleave", function () {
+        resetHover();
+    });
+
+    $("#header")
+        .on("mouseenter", function () {
+        // reset flag
+        hovering = true;
+        // reset timeout
+        startTimeout();
+    })
+        .on("mouseleave", function () {
+        // The timeout is needed incase you go back to the main menu
+        resetHover();
+    });
+
+    function startTimeout() {
+        // This method gives you 1 second to get your mouse to the sub-menu
+        timeout = setTimeout(function () {
+            closeMenu();
+        }, 1000);
+    };
+
+    function closeMenu() {
+        // Only close if not hovering
+        if (!hovering) {
+            $('#header').stop(true, true).slideUp(400);
+        }
+    };
+
+    function resetHover() {
+        // Allow the menu to close if the flag isn't set by another event
+        hovering = false;
+        // Set the timeout
+        startTimeout();
+    };
 });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+  	session[:numero] ||= 4 
+  	session[:numero] = 2 if params[:numero2]
 	session[:produto1] = params[:produto1] if params[:produto1]
 	session[:produto2] = params[:produto2] if params[:produto2]
 	session[:produto3] = params[:produto3] if params[:produto3]
 	session[:produto4] = params[:produto4] if params[:produto4]
 
+	@numero = session[:numero]
+	@two = "two_products" if @numero == 2
+	@hide = "true" if @numero == 2
 	@produto1 = Produto.find_by(nome: session[:produto1]) if session[:produto1]
 	@produto2 = Produto.find_by(nome: session[:produto2]) if session[:produto2]
 	@produto3 = Produto.find_by(nome: session[:produto3]) if session[:produto3]
