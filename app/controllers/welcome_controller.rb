d4537870ecea6468ece445a810d8537cb138b279
Anike Arni <anikarni@anikarni-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-06-17 13:13:26 -0300

    implemented autocomplete

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	session[:numero] ||= 4 
   	session[:numero] = 2 if params[:numero2]
+  	session[:numero] = 4 if params[:numero4]
 	session[:produto1] = params[:produto1] if params[:produto1]
 	session[:produto2] = params[:produto2] if params[:produto2]
 	session[:produto3] = params[:produto3] if params[:produto3]
@@ -10,6 +11,9 @@ class WelcomeController < ApplicationController
 	@numero = session[:numero]
 	@two = "two_products" if @numero == 2
 	@hide = "true" if @numero == 2
+	@two = "" if @numero == 4
+	@hide = "" if @numero == 4
+	@produtos = Produto.all
 	@produto1 = Produto.find_by(nome: session[:produto1]) if session[:produto1]
 	@produto2 = Produto.find_by(nome: session[:produto2]) if session[:produto2]
 	@produto3 = Produto.find_by(nome: session[:produto3]) if session[:produto3]
