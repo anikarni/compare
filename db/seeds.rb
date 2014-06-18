a527ef70f5990de56fd336cbcbb433b843e59118
anikarni <anikarni@gmail.com>
2014-06-18 17:57:17 -0300

    Photo in assets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
 #   Mayor.create(name: "Emanuel", city: cities.first)
-Produto.create(nome: "Camiseta Terno", modelo: "Casual", preco: "R$23,40", tamanhos: "P, M, G", cores: "preto", quantidade: "5", descricao: "Produto contém estampa de terno solto na frente", foto: File.new("test/fixtures/camiseta1.jpg"))
-Produto.create(nome: "Camiseta Calendário", modelo: "Casual", preco: "R$40,00", tamanhos: "M, G", cores: "branco, vermelha", quantidade: "34", descricao: "Produto contém estampa de calendário na frente e inclui caneta para escrever na camiseta", foto: File.new("test/fixtures/camiseta2.jpg"))
-Produto.create(nome: "Camiseta Gato Preto", modelo: "Casual", preco: "R$20,00", tamanhos: "P, M, G", cores: "branco, verde, azul", quantidade: "32", descricao: "Produto contém estampa de gato preto com frase 'Black cats rule'", foto: File.new("test/fixtures/camiseta3.jpg"))
-Produto.create(nome: "Camiseta Tuxedo", modelo: "Casual", preco: "R$30,00", tamanhos: "P, G", cores: "laranja, preto, azul, vermelha", quantidade: "12", descricao: "Produto contém estampa de terno com gravata, flor, e bolsos", foto: File.new("test/fixtures/camiseta4.jpg"))
+Produto.create(nome: "Camiseta Terno", modelo: "Casual", preco: "R$23,40", tamanhos: "P, M, G", cores: "preto", quantidade: "5", descricao: "Produto contém estampa de terno solto na frente", foto: File.new("app/assets/images/camiseta1.jpg"))
+Produto.create(nome: "Camiseta Calendário", modelo: "Casual", preco: "R$40,00", tamanhos: "M, G", cores: "branco, vermelha", quantidade: "34", descricao: "Produto contém estampa de calendário na frente e inclui caneta para escrever na camiseta", foto: File.new("app/assets/images/camiseta2.jpg"))
+Produto.create(nome: "Camiseta Gato Preto", modelo: "Casual", preco: "R$20,00", tamanhos: "P, M, G", cores: "branco, verde, azul", quantidade: "32", descricao: "Produto contém estampa de gato preto com frase 'Black cats rule'", foto: File.new("app/assets/images/camiseta3.jpg"))
+Produto.create(nome: "Camiseta Tuxedo", modelo: "Casual", preco: "R$30,00", tamanhos: "P, G", cores: "laranja, preto, azul, vermelha", quantidade: "12", descricao: "Produto contém estampa de terno com gravata, flor, e bolsos", foto: File.new("app/assets/images/camiseta4.jpg"))
